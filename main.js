76b39be86b2e042110141cd34510be3c4966f90c
Shivam Pai <72920537+shivampai@users.noreply.github.com>
2021-12-16 15:54:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-showNotification();
+randNO = Math.floor(Math.random()*999);
+document.getElementById('mid').value = randNO;
 function showNotification() {
     const notification = new Notification("Message From Naturals Maintainence", {
         body: 'Click Here To Open',
@@ -6,7 +7,7 @@ function showNotification() {
     })
 }
 function openPage(sectno) {
-    sects = [0, 1, 2, 3, 4]
+    sects = [0, 1, 2, 3, 4,5]
     for (i = 0; i < sects.length; i++) {
         document.getElementById(i).style.display = 'none';
     }
@@ -16,7 +17,7 @@ function change(floorid, dep, machine) {
     firebase.database().ref('machine/' + floorid + '/' + dep + '/' + machine).once('value', (snapshot) => {
         const data = snapshot.val();
         console.log('machine/' + floorid + '/' + dep + '/' + machine);
-        detailsHTML = '<span><b>Machine Name:</b>&nbsp;<span>' + data.details.name + '</span></span><br><span><b>Average Rating:</b>&nbsp;<span>' + data.details.average + '</span></span><hr>';
+        detailsHTML = '<span><b>Machine Name:</b>&nbsp;<span>' + data.details.name + '</span></span><hr>';
         detailsHTML += '<button class="button" onclick="openPage(2)">Rate Machine</button><button class="button" onclick="openPage(3)">Edit Details</button><button class="button" onclick="openPage(4)">Get Ratings</button>'
         document.getElementById('details').innerHTML = detailsHTML;
     });
@@ -31,46 +32,153 @@ function notification() {
     }
 }
 function dateChange() {
-    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/daily/' + document.getElementById('getDate').value + '/param').once('value', (snapshot) => {
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/daily/' + document.getElementById('getDate').value).once('value', (snapshot) => {
         const data = snapshot.val();
         console.log(data);
         if (!(data)) {
             document.getElementById('date-param').innerHTML = 'Not Registered For The Selected Date'
         } else {
             document.getElementById('date-param').innerHTML = '';
-            for (i = 0; i < data.length; i++) {
-                document.getElementById('date-param').innerHTML += data[i].placeholder + ' : ' + data[i].value + '<hr>';
+            document.getElementById('day-time').innerHTML = 'Submitted At : '+data.time;
+            for (i = 0; i < data.param.length; i++) {
+                document.getElementById('date-param').innerHTML += data.param[i].placeholder + ' : ' + data.param[i].value + '<hr>';
             }
         }
     });
 }
 
 function monthChange() {
-    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/monthly/' + document.getElementById('getmonth').value + '/param').once('value', (snapshot) => {
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/monthly/' + document.getElementById('getmonth').value).once('value', (snapshot) => {
         const data = snapshot.val();
         console.log(data);
         if (!(data)) {
             document.getElementById('month-param').innerHTML = 'Not Registered For The Selected Date'
         } else {
             document.getElementById('month-param').innerHTML = '';
-            for (i = 0; i < data.length; i++) {
-                document.getElementById('month-param').innerHTML += data[i].placeholder + ' : ' + data[i].value + '<hr>';
+            document.getElementById('month-time').innerHTML = 'Submitted At : '+data.time;
+            for (i = 0; i < data.param.length; i++) {
+                document.getElementById('month-param').innerHTML += data.param[i].placeholder + ' : ' + data.param[i].value + '<hr>';
             }
         }
     });
 }
 
 function weekChange() {
-    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/weekly/' + document.getElementById('getWeek').value + '/param').once('value', (snapshot) => {
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/weekly/' + document.getElementById('getWeek').value).once('value', (snapshot) => {
         const data = snapshot.val();
         console.log(data);
         if (!(data)) {
             document.getElementById('week-param').innerHTML = 'Not Registered For The Selected Date'
         } else {
             document.getElementById('week-param').innerHTML = '';
-            for (i = 0; i < data.length; i++) {
-                document.getElementById('week-param').innerHTML += data[i].placeholder + ' : ' + data[i].value + '<hr>';
+            document.getElementById('week-time').innerHTML = 'Submitted At : '+data.time;
+            for (i = 0; i < data.param.length; i++) {
+                document.getElementById('week-param').innerHTML += data.param[i].placeholder + ' : ' + data.param[i].value + '<hr>';
             }
         }
     });
+}
+paramlist = 0;
+function showNewParam() {
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/details/params/daily').once('value', (snapshot) => {
+        const data = snapshot.val();
+        var params = document.getElementById('new-daily-param');
+        params.innerHTML = '';
+        paramlist = data.length;
+        for (i = 0; i < data.length; i++) {
+            params.innerHTML += '<input type="text" id="param-input-' + i + '" placeholder="' + data[i] + '">';
+        }
+
+    });
+}
+function showNewParamMonth() {
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/details/params/monthly').once('value', (snapshot) => {
+        const data = snapshot.val();
+        var params = document.getElementById('new-monthly-param');
+        params.innerHTML = '';
+        paramlist = data.length;
+        for (i = 0; i < data.length; i++) {
+            params.innerHTML += '<input type="text" id="param-month-input-' + i + '" placeholder="' + data[i] + '">';
+        }
+
+    });
+}
+
+function showNewParamWeekly() {
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/details/params/weekly').once('value', (snapshot) => {
+        const data = snapshot.val();
+        var params = document.getElementById('new-weekly-param');
+        params.innerHTML = '';
+        paramlist = data.length;
+        for (i = 0; i < data.length; i++) {
+            params.innerHTML += '<input type="text" id="param-week-input-' + i + '" placeholder="' + data[i] + '">';
+        }
+
+    });
+}
+
+function submitNewDaily() {
+    submitParam = [];
+    for (i = 0; i < paramlist; i++) {
+        submitParam.push({ value: document.getElementById('param-input-' + i).value, placeholder: document.getElementById('param-input-' + i).placeholder });
+    }
+    console.log(submitParam);
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/daily/' + document.getElementById('getDate2').value).set({
+        param: submitParam,
+        time: document.getElementById('getTimeInDate').value
+    });
+    openPage(1);
+}
+function submitNewMonthly() {
+    submitParam = [];
+    for (i = 0; i < paramlist; i++) {
+        submitParam.push({ value: document.getElementById('param-month-input-' + i).value, placeholder: document.getElementById('param-month-input-' + i).placeholder });
+    }
+    console.log(submitParam);
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/monthly/' + document.getElementById('getMonth2').value).set({
+        param: submitParam,
+        time: document.getElementById('getDateTimeMonth').value
+    });
+    openPage(1);
+}
+function submitNewWeekly() {
+    submitParam = [];
+    for (i = 0; i < paramlist; i++) {
+        submitParam.push({ value: document.getElementById('param-week-input-' + i).value, placeholder: document.getElementById('param-week-input-' + i).placeholder });
+    }
+    console.log(submitParam);
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/weekly/' + document.getElementById('getWeek2').value).set({
+        param: submitParam,
+        time: document.getElementById('getDateTimeWeek').value
+    });
+    openPage(1);
+}
+function updateDetails(){
+    firebase.database().ref('machine/' + sessionStorage.getItem('floor') + '/' + sessionStorage.getItem('dep') + '/' + sessionStorage.getItem('machine') + '/details/name').set(document.getElementById('changedetails-machine-name').value);
+    openPage(1);
+    change(sessionStorage.getItem('floor') , sessionStorage.getItem('dep') , sessionStorage.getItem('machine'))
+}
+function newMachine(){
+firebase.database().ref('machine/' + document.getElementById('new-floor').value + '/'+document.getElementById('selector-newdid').value+'/'+document.getElementById('mid').value+ '/details').set({
+    params:{daily:newMachineParam,weekly:newWeeklyParam,monthly:newMonthlyParam},
+    name:document.getElementById('screen5-machine-name').value
+})
+}
+newMachineParam = [];
+function submitNewParam(newParam){
+    document.getElementById('list').innerHTML += newParam + '<hr>';
+    newMachineParam.push(newParam)
+    document.getElementById('screen5-machine-newparam').value = '';
+}
+newWeeklyParam = [];
+function submitNewWeekParam(newParam){
+    document.getElementById('week-list').innerHTML += newParam + '<hr>';
+    newWeeklyParam.push(newParam)
+    document.getElementById('screen5-week-machine-newparam').value = '';
+}
+newMonthlyParam = [];
+function submitNewMonthParam(newParam){
+    document.getElementById('month-list').innerHTML += newParam + '<hr>';
+    newMonthlyParam.push(newParam)
+    document.getElementById('screen5-month-machine-newparam').value = '';
 }
